d7112a92b7c7a1fb1b2c29707fb2396ecad1d8b1
SergioHPassos <shp360@hotmail.com>
2022-05-13 19:57:48 +0900

    get pilot data on mount

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,44 @@
+import React, { useState, useEffect } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
 // components
 import ListMission from '../components/ListMission'
+import Profile from '../components/Profile'
 
 const Home: NextPage = () => {
+  const [pilot, setPilot] = useState<Pilot[]>(null)
+
+  useEffect(() => {
+    getPilot(setPilot)
+  }, [])
+
   return (
     <div className="">
-      <ListMission />
+      <Profile pilots={pilot} setPilot={setPilot} />
     </div>
   )
 }
 
 export default Home
+
+// GET request for pilot
+const getPilot = async (setPilot: Function) => {
+  const id: string = 'cl347rys00013ysutsmhnpzlr'
+  await fetch(`/api/pilot/${id}`)
+    .then((res) => res.json())
+    .then((pilots) => setPilot(pilots))
+}
+
+interface Pilot {
+  id: string
+  firstName: string
+  lastName: string
+  middleName: string
+  age: number
+  money: number
+  totalDistance: number
+  totalCargo: number
+  totalPassenger: number
+}

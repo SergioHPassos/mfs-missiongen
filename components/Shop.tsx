976fd06de094b550c9ad4c2b8f308b69d20c4e03
Sergio Passos <shp360@hotmail.com>
2022-05-14 23:26:09 +0900

    added cost next to buy button

--- components/Shop.tsx
+++ components/Shop.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Pilot, Plane } from '../interfaces/UtilityInterfaces'
 import ShopList from './ShopList'
 
 export default function Shop(props: Props) {
+  const [totalShoppingCartCost, setTotalShoppingCartCost] = useState<number>(0)
+
   return (
     <>
       {/* title */}
@@ -18,15 +20,28 @@ export default function Shop(props: Props) {
             return (
               <>
                 {/* aircraft name and cost */}
-                <ShopList plane={plane} />
+                <ShopList
+                  plane={plane}
+                  totalShoppingCartCost={totalShoppingCartCost}
+                  setTotalShoppingCartCost={setTotalShoppingCartCost}
+                />
               </>
             )
           })}
 
         {/* buy plane button */}
         <div className="flex justify-center pt-8 text-center">
-          <button className="h-10 w-24 rounded-sm bg-yellow-500 text-xl text-yellow-100">
-            Buy
+          <button
+            className={
+              totalShoppingCartCost > 0
+                ? 'text-md h-10 w-36 rounded-sm bg-yellow-500 text-yellow-100'
+                : 'h-10 w-24 rounded-sm bg-yellow-500 text-xl text-yellow-100'
+            }
+            onClick={() => {}}
+          >
+            {totalShoppingCartCost > 0
+              ? `Buy ($${totalShoppingCartCost})`
+              : 'Buy'}
           </button>
         </div>
       </div>

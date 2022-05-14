fe72061433bfcb95714a9fc1c55cfe22e6c2fc64
SergioHPassos <shp360@hotmail.com>
2022-05-14 13:01:12 +0900

    removed pilot data

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,33 +3,8 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
-// components
-import ListMission from '../components/ListMission'
-import Profile from '../components/Profile'
-
-// interfaces
-import type { Pilot } from '../interfaces/UtilityInterfaces'
-
 const Home: NextPage = () => {
-  const [pilot, setPilot] = useState<Pilot[]>(null)
-
-  useEffect(() => {
-    getPilot(setPilot)
-  }, [])
-
-  return (
-    <div className="">
-      <Profile pilots={pilot} setPilot={setPilot} />
-    </div>
-  )
+  return <div className="">asdasdasdasdasd</div>
 }
 
 export default Home
-
-// GET request for pilot
-const getPilot = async (setPilot: Function) => {
-  const id: string = 'cl347rys00013ysutsmhnpzlr'
-  await fetch(`/api/pilot/${id}`)
-    .then((res) => res.json())
-    .then((pilots) => setPilot(pilots))
-}

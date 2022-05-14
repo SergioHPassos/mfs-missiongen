81cbf5af9789478b4d957e7e64f83f5243f0ce9b
SergioHPassos <shp360@hotmail.com>
2022-05-14 13:01:00 +0900

    added new page

--- components/Header.tsx
+++ components/Header.tsx
@@ -72,7 +72,7 @@ export default function Header() {
           </p>
         </Link>
 
-        <Link href="" passHref={true}>
+        <Link href="/pilot" passHref={true}>
           <p
             className="py-[0.35rem] text-lg font-medium"
             onClick={toggleIsDropped}

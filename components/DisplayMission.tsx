511d2464a9359ece6c49b1fc2f06db55b560b3eb
SergioHPassos <shp360@hotmail.com>
2022-05-15 13:28:29 +0900

    map and list mission component

--- components/DisplayMission.tsx
+++ components/DisplayMission.tsx
@@ -1,5 +1,26 @@
+import type { Mission } from '@prisma/client'
 import React from 'react'
 
-export default function DisplayMission() {
-  return <div>DisplayMission</div>
+// dependencies
+import { v4 as uuidv4 } from 'uuid'
+
+// components
+import Title from './Title'
+import MissionItem from './MissionItem'
+
+export default function DisplayMission(props: Props) {
+  return (
+    <>
+      <Title title="Missions" />
+      <div className="flex flex-col pt-14">
+        {props.missions.map((mission) => {
+          return <MissionItem mission={mission} key={uuidv4()} />
+        })}
+      </div>
+    </>
+  )
+}
+
+interface Props {
+  missions?: Mission[]
 }

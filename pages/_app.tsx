433fcfd2028858c8d6e8f8fb430046639ee07480
SergioHPassos <shp360@hotmail.com>
2022-05-15 17:05:35 +0900

    install context provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,31 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
+import { Pilot } from '@prisma/client'
+
+// components
 import Header from '../components/Header'
 
+// context
+import { PilotContext } from '../context/PilotContext'
+
 function MyApp({ Component, pageProps }: AppProps) {
+  // pilot profile hook
+  const [pilot, setPilot] = useState<Pilot | null>(null)
+
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <PilotContext.Provider
+        value={{
+          state: {
+            pilot: pilot,
+            setPilot: setPilot,
+          },
+        }}
+      >
+        <Header />
+        <Component {...pageProps} />
+      </PilotContext.Provider>
     </>
   )
 }

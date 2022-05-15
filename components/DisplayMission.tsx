3c553c447778d930a4c2efe368a98005778643f7
SergioHPassos <shp360@hotmail.com>
2022-05-15 17:58:01 +0900

    WIP modal

--- components/DisplayMission.tsx
+++ components/DisplayMission.tsx
@@ -11,6 +11,17 @@ import MissionItem from './MissionItem'
 export default function DisplayMission(props: Props) {
   return (
     <>
+      {true && (
+        <>
+          <div className="fixed inset-0 left-1/2 top-1/2 z-50 h-[13%] w-[30%] -translate-x-1/2 -translate-y-1/2 bg-zinc-200">
+            <p>Select Flight?</p>
+            <div className="">
+              <button>Accept</button>
+              <button></button>
+            </div>
+          </div>
+        </>
+      )}
       <Title title="Missions" />
       <div className="flex flex-col pt-14">
         {props.missions.map((mission) => {

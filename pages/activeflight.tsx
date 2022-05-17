e7b8be0a66cdaa0d24e8055ae4f1fbae749701d6
SergioHPassos <shp360@hotmail.com>
2022-05-17 13:06:56 +0900

    page for displaying current mission

--- pages/activeflight.tsx
+++ pages/activeflight.tsx
@@ -1,5 +1,54 @@
-import React from 'react'
+import { Mission, Pilot } from '@prisma/client'
+import { stat } from 'fs'
+import React, { useContext } from 'react'
+
+// components
+import ActiveFlight from '../components/ActiveFlight'
+import MissionItem from '../components/MissionItem'
+
+// context provider
+import { PilotContext } from '../context/PilotContext'
 
 export default function activeflight() {
-  return <div>activeflight</div>
+  const { state } = useContext(PilotContext)
+
+  return (
+    <>
+      <div className="">
+        <ActiveFlight
+          mission={state.mission}
+          updatePilot={() => {
+            const pilot = {
+              ...state.pilot,
+              money: state.pilot.money + state.mission.reward,
+              totalDistance: state.pilot.totalDistance + state.mission.distance,
+              totalCargo:
+                state.mission.type === 'Deliver'
+                  ? state.pilot.totalCargo + state.mission.objectiveQuantity
+                  : state.pilot.totalCargo,
+              totalPassenger:
+                state.mission.type === 'Drop off'
+                  ? state.pilot.totalPassenger + state.mission.objectiveQuantity
+                  : state.pilot.totalPassenger,
+            }
+
+            state.setPilot(pilot)
+
+            // state.setMission(null)
+
+            patchPilot(state.pilot)
+          }}
+        />
+      </div>
+    </>
+  )
+}
+
+const patchPilot = async (pilot: Pilot) => {
+  const id: string = 'cl347rys00013ysutsmhnpzlr'
+  const res = await fetch(`http://localhost:3000/api/pilot/${id}`, {
+    method: 'PATCH',
+    body: JSON.stringify(pilot),
+  })
+  console.log(res)
 }

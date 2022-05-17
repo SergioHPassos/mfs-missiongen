11e54d3c56c55dd12886ddf8471498aa65ccd63b
SergioHPassos <shp360@hotmail.com>
2022-05-17 13:04:22 +0900

    removed modal

--- components/DisplayMission.tsx
+++ components/DisplayMission.tsx
@@ -1,5 +1,5 @@
 import type { Mission } from '@prisma/client'
-import React from 'react'
+import React, { useState } from 'react'
 
 // dependencies
 import { v4 as uuidv4 } from 'uuid'
@@ -7,21 +7,13 @@ import { v4 as uuidv4 } from 'uuid'
 // components
 import Title from './Title'
 import MissionItem from './MissionItem'
+import Modal from './Modal'
 
 export default function DisplayMission(props: Props) {
+  const [isActive, setIsActive] = useState<boolean>(false)
+
   return (
     <>
-      {true && (
-        <>
-          <div className="fixed inset-0 left-1/2 top-1/2 z-50 h-[13%] w-[30%] -translate-x-1/2 -translate-y-1/2 bg-zinc-200">
-            <p>Select Flight?</p>
-            <div className="">
-              <button>Accept</button>
-              <button></button>
-            </div>
-          </div>
-        </>
-      )}
       <Title title="Missions" />
       <div className="flex flex-col pt-14">
         {props.missions.map((mission) => {

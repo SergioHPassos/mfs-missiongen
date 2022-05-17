47240c15f56494181b18e4f23780972937fff1a0
SergioHPassos <shp360@hotmail.com>
2022-05-17 13:14:53 +0900

    fixed key bug

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -13,35 +13,36 @@ export default function Profile(props: Props) {
   return (
     <>
       <Title title="Pilot" />
-      <div className="flex items-center justify-between px-3 pt-14">
-        {props.pilots &&
-          props.pilots.map((pilot) => {
-            return (
-              <>
-                <div className="flex flex-col space-y-4" key={uuidv4()}>
-                  <p>Pilot:</p>
-                  <p>Age:</p>
-                  <p>Money:</p>
-                  <p>Distance Traveled:</p>
-                  <p>Total Cargo:</p>
-                  <p>Total Passenger:</p>
-                  <p>Missions Completed:</p>
-                  <p>Planes Owned:</p>
-                </div>
-                <div className="flex flex-col space-y-4" key={uuidv4()}>
-                  <p>{`${pilot.firstName} ${pilot.middleName[0]}. ${pilot.lastName}`}</p>
-                  <p>{`${pilot.age}`}</p>
-                  <p>{`$${pilot.money}`}</p>
-                  <p>{`${pilot.totalDistance}`}</p>
-                  <p>{`${pilot.totalCargo}lb`}</p>
-                  <p>{`${pilot.totalPassenger}`}</p>
-                  <p>{`${pilot.missions.length}`}</p>
-                  <p>{`${pilot.planes.length}`}</p>
-                </div>
-              </>
-            )
-          })}
-      </div>
+      {props.pilots &&
+        props.pilots.map((pilot) => {
+          return (
+            <div
+              className="flex items-center justify-between px-3 pt-14"
+              key={uuidv4()}
+            >
+              <div className="flex flex-col space-y-4">
+                <p>Pilot:</p>
+                <p>Age:</p>
+                <p>Money:</p>
+                <p>Distance Traveled:</p>
+                <p>Total Cargo:</p>
+                <p>Total Passenger:</p>
+                <p>Missions Completed:</p>
+                <p>Planes Owned:</p>
+              </div>
+              <div className="flex flex-col space-y-4">
+                <p>{`${pilot.firstName} ${pilot.middleName[0]}. ${pilot.lastName}`}</p>
+                <p>{`${pilot.age}`}</p>
+                <p>{`$${pilot.money}`}</p>
+                <p>{`${pilot.totalDistance}`}</p>
+                <p>{`${pilot.totalCargo}lb`}</p>
+                <p>{`${pilot.totalPassenger}`}</p>
+                <p>{`${pilot.missions.length}`}</p>
+                <p>{`${pilot.planes.length}`}</p>
+              </div>
+            </div>
+          )
+        })}
     </>
   )
 }

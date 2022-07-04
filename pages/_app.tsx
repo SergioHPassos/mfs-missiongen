388ef15d1a1ded6e8aa6220e50ea690241d00e27
Sergio Passos <shp360@hotmail.com>
2022-07-04 20:47:30 +0900

    added session on the main app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react'
 import { Pilot, Mission } from '@prisma/client'
 import { ThemeProvider } from 'next-themes'
+import { SessionProvider } from 'next-auth/react'
 
 // components
 import Header from '../components/Header'
@@ -10,7 +11,7 @@ import Header from '../components/Header'
 // context
 import { PilotContext } from '../context/PilotContext'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   // pilot profile hook
   const [pilot, setPilot] = useState<Pilot | null>(null)
   const [mission, setMission] = useState<Mission | null>(null)
@@ -25,25 +26,27 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <div>
-      <ThemeProvider>
-        <PilotContext.Provider
-          value={{
-            state: {
-              pilot: pilot,
-              setPilot: setPilot,
-              mission: mission,
-              setMission: setMission,
-              activeMission: activeMission,
-              setActiveMission: setActiveMission,
-              accountBalance: accountBalance,
-              setAccountBalance: setAccountBalance,
-            },
-          }}
-        >
-          <Header />
-          <Component {...pageProps} />
-        </PilotContext.Provider>
-      </ThemeProvider>
+      <SessionProvider session={session}>
+        <ThemeProvider>
+          <PilotContext.Provider
+            value={{
+              state: {
+                pilot: pilot,
+                setPilot: setPilot,
+                mission: mission,
+                setMission: setMission,
+                activeMission: activeMission,
+                setActiveMission: setActiveMission,
+                accountBalance: accountBalance,
+                setAccountBalance: setAccountBalance,
+              },
+            }}
+          >
+            <Header />
+            <Component {...pageProps} />
+          </PilotContext.Provider>
+        </ThemeProvider>
+      </SessionProvider>
     </div>
   )
 }

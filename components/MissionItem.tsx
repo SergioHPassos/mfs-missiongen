357e6e61074ea150d254bf480b56bdb1d5ea8854
Sergio Passos <shp360@hotmail.com>
2022-07-05 21:45:54 +0900

    update text color

--- components/MissionItem.tsx
+++ components/MissionItem.tsx
@@ -118,7 +118,7 @@ function CheckIcon(props: {
         props.setModalMissionData({ ...props.mission })
         props.openModal()
       }}
-      className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-base-300 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+      className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
     >
       Details
     </button>
